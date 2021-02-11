0c563d5f808ee7ee7fcce4ccbf88cee71faf5d24
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 19:27:09 +0530

    Second Commit

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -4,9 +4,11 @@ public class SnakeAndLadderUC1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int pos_of_player=0;                
-        System.out.println(pos_of_player);
-
+		int pos_of_player=0;
+        int no_on_die = 1+(int)Math.floor(Math.random()*10)%6;                
+            System.out.println(pos_of_player + " " + no_on_die);
+        }
+        
 	}
 
-}
+
