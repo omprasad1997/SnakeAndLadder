54699ebd9c946c2a5ac94ffca54f6a2efdc521b1
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 19:43:44 +0530

    Third Commit

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -6,22 +6,29 @@ public class SnakeAndLadderUC1 {
 		public static final int snake   = 2;
 
 	public static void main(String[] args) {
-		 int pos_of_player=1;
-	        int no_on_die = 1+(int)Math.floor(Math.random()*10)%6;                
-	            
-	        int options = (int)Math.floor(Math.random()*10)%3;
-	        switch(options)
-	        {
-	            case no_play :
-	                break;
-	            case ladder:
-	                pos_of_player = pos_of_player + no_on_die;
-	                break;
-	            case snake:
-	                pos_of_player = pos_of_player - no_on_die;
-	                break; 
+		int pos_of_player=0;
 
-	        }
+        while(pos_of_player!=100)
+        {
+            if(pos_of_player < 0)
+                pos_of_player = 0;
+            else
+            {
+                int no_on_die = 1+(int)Math.floor(Math.random()*10)%6;                    
+                int options = (int)Math.floor(Math.random()*10)%3;
+                switch(options)
+                {
+                    case no_play :
+                        break;
+                    case ladder:
+                        pos_of_player = pos_of_player + no_on_die;
+                        break;
+                    case snake:
+                        pos_of_player = pos_of_player - no_on_die;
+                        break; 
+                }
+            }
+        }
 	}
 }
 
