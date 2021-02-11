a81cddccd536803c655ecc4e3b2f8d76a6a6539c
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 19:55:17 +0530

    Fifth Commit

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -7,32 +7,38 @@ public class SnakeAndLadderUC1 {
 
 	public static void main(String[] args) {
 		int pos_of_player=0;
+	    int count_of_dice=0;
+	    int count_of_position = 0;
 
-        while(pos_of_player!=100)
-        {
-            if(pos_of_player < 0)
-                pos_of_player = 0;
-            else
-            {
-                int no_on_die = 1+(int)Math.floor(Math.random()*10)%6;                    
-                int options = (int)Math.floor(Math.random()*10)%3;
-                switch(options)
-                {
-                    case no_play :
-                        break;
-                    case ladder:
-                        if(pos_of_player + no_on_die > 100)
-                            break;
-                        else
-                            pos_of_player = pos_of_player + no_on_die;
-
-                        break;
-                    case snake:
-                        pos_of_player = pos_of_player - no_on_die;
-                        break; 
-                }
-            }
-        } 
+	    while(pos_of_player!=100)
+	    {
+	        if(pos_of_player < 0)
+	            pos_of_player = 0;
+	        else
+	        {
+	            int no_on_die = 1+(int)Math.floor(Math.random()*10)%6; 
+	            count_of_dice++;
+	            
+	            int options = (int)Math.floor(Math.random()*10)%3;
+	            switch(options)
+	            {
+	                case no_play :
+	                    break;
+	                case ladder:
+	                    if(pos_of_player + no_on_die > 100)
+	                        break;
+	                    else{
+	                        pos_of_player = pos_of_player + no_on_die;
+	                        count_of_position++;
+	                    }
+	                    break;
+	                case snake:
+	                    pos_of_player = pos_of_player - no_on_die;
+	                    break; 
+	            }
+	        }
+	    }   
+	    System.out.println(count_of_dice + " " + count_of_position); 
 	}
 }
 
