30577fbf065ed890f4d2f1f6f0cf8c2b24a65a04
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 19:20:32 +0530

    First Commit

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,12 @@
+package com.blbz;
+
+public class SnakeAndLadderUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pos_of_player=0;                
+        System.out.println(pos_of_player);
+
+	}
+
+}
