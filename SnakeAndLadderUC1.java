f5d78fbc6ea2fc604a08f0a02ca6254b39343355
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 19:33:59 +0530

    Second Commit

--- SnakeAndLadderUC1.java
+++ SnakeAndLadderUC1.java
@@ -1,14 +1,28 @@
 package com.blbz;
 
 public class SnakeAndLadderUC1 {
+		public static final int no_play = 0;
+		public static final int ladder  = 1;
+		public static final int snake   = 2;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int pos_of_player=0;
-        int no_on_die = 1+(int)Math.floor(Math.random()*10)%6;                
-            System.out.println(pos_of_player + " " + no_on_die);
-        }
-        
+		 int pos_of_player=1;
+	        int no_on_die = 1+(int)Math.floor(Math.random()*10)%6;                
+	            
+	        int options = (int)Math.floor(Math.random()*10)%3;
+	        switch(options)
+	        {
+	            case no_play :
+	                break;
+	            case ladder:
+	                pos_of_player = pos_of_player + no_on_die;
+	                break;
+	            case snake:
+	                pos_of_player = pos_of_player - no_on_die;
+	                break; 
+
+	        }
 	}
+}
 
 
